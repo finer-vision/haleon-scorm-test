94a6f204f1b0ed273e4f6504f9d5dabafdf73128
Enijar <TheEnijar@gmail.com>
2023-12-05 14:12:39 +0000

    relear: 0.0.14

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -11,14 +11,6 @@ export default function Landing() {
     <main>
       <h1>1/3 Landing Page</h1>
       <Link to="/home">Go to Home page</Link>
-      <video
-        src="https://chkgcampus.sapjam.com/groups/R584Kafq5dKUSLY6U5D3Yp/documents/chIf04wz1GWPGzAye77QJR/video_viewer"
-        controls
-      />
-      <video
-        src="https://haleon.sharepoint.com/:v:/s/mywinningcareer/Ed0hCYeBK71Kl-0c9lIvgt8BPSSfPR1miO-FE2NH2Sb53A?e=FSBhj1"
-        controls
-      />
     </main>
   );
 }
