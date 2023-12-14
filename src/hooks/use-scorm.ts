b7d34d17e846c396e615431121bdd6514ae79316
Enijar <TheEnijar@gmail.com>
2023-12-14 16:11:54 +0000

    refactor: move scorm api code to own service file

--- src/hooks/use-scorm.ts
+++ src/hooks/use-scorm.ts
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-// import { scorm } from "@gamestdio/scorm";
+import { scorm } from "@/services/scorm";
 
 type Bookmark = {
   pathname: string;
@@ -12,105 +12,36 @@ type Scorm = {
   exit: () => void;
 };
 
-const API = ((window as any).opener.top["API"] as any) ?? null;
-
-(window as any).API = API;
-
 export const useScorm = create<Scorm>(() => {
-  function set(key: string, value: any) {
-    // if (!scorm.isActive) {
-    //   return;
-    // }
-    // scorm.set(key, key === "cmi.suspend_data" ? JSON.stringify(value) : value);
-    if (API === null) return;
-    API["LMSSetValue"](key, key === "cmi.suspend_data" ? JSON.stringify(value) : value);
-  }
-
-  function get(key: string, defaultValue?: any) {
-    // if (!scorm.isActive) {
-    //   return defaultValue;
-    // }
-    // if (key === "cmi.suspend_data") {
-    //   try {
-    //     const value = scorm.get(key);
-    //     if (value === "") {
-    //       return defaultValue;
-    //     }
-    //     return JSON.parse(value) ?? defaultValue;
-    //   } catch {
-    //     return defaultValue;
-    //   }
-    // }
-    // const value = scorm.get(key);
-    // if (value === "") {
-    //   return defaultValue;
-    // }
-    // return value;
-    if (API === null) return;
-    const value = API["LMSGetValue"](key);
-    if (key === "cmi.suspend_data") {
-      try {
-        if (value === "") {
-          return defaultValue;
-        }
-        return JSON.parse(value) ?? defaultValue;
-      } catch {
-        return defaultValue;
-      }
-    }
-    if (value === "") {
-      return defaultValue;
-    }
-    return value;
-  }
-
-  function commit() {
-    // scorm.commit();
-    if (API === null) return;
-    API["LMSCommit"]("");
-  }
-
-  function terminate() {
-    // if (!scorm.isActive) {
-    //   return;
-    // }
-    // scorm.terminate();
-    if (API === null) return;
-    API["LMSFinish"]("");
-  }
-
   return {
     async init() {
-      // scorm.configure({ debug: true, handleExitMode: true, handleCompletionStatus: false, version: "1.2" });
-      // scorm.initialize();
-      API["LMSInitialize"]("");
+      scorm.initialize();
       console.log("SCORM initialized");
-      set("cmi.core.score.min", 0);
-      set("cmi.core.score.max", 1);
-      const progress = parseFloat(get("cmi.core.score.raw", "0"));
+      scorm.set("cmi.core.score.min", 0);
+      scorm.set("cmi.core.score.max", 1);
+      const progress = parseFloat(scorm.get("cmi.core.score.raw", "0"));
       if (progress < 1) {
-        set("cmi.completion_status", "incomplete");
-        set("cmi.success_status", "unknown");
+        scorm.set("cmi.completion_status", "incomplete");
+        scorm.set("cmi.success_status", "unknown");
       }
-      commit();
-      return get("cmi.suspend_data", { pathname: "/" });
+      scorm.commit();
+      return scorm.get("cmi.suspend_data", { pathname: "/" });
     },
     setBookmark(bookmark) {
-      set("cmi.suspend_data", bookmark);
-      commit();
+      scorm.set("cmi.suspend_data", bookmark);
+      scorm.commit();
     },
     setProgress(progress) {
-      // if (!scorm.isActive) return;
-      progress = Math.max(parseFloat(get("cmi.core.score.raw", "0")), progress);
-      set("cmi.core.score.raw", progress);
+      progress = Math.max(parseFloat(scorm.get("cmi.core.score.raw", "0")), progress);
+      scorm.set("cmi.core.score.raw", progress);
       if (progress === 1) {
-        set("cmi.completion_status", "completed");
-        set("cmi.success_status", "passed");
+        scorm.set("cmi.completion_status", "completed");
+        scorm.set("cmi.success_status", "passed");
       }
-      commit();
+      scorm.commit();
     },
     exit() {
-      terminate();
+      scorm.terminate();
       window.close();
     },
   };
---
+++ src/services/scorm.ts
@@ -0,0 +1,51 @@
+const SCORM_API = ((window as any).opener.top["API"] as any) ?? null;
+
+// For debugging
+(window as any).SCORM_API = SCORM_API;
+
+export const scorm = {
+  initialize() {
+    if (SCORM_API === null) {
+      return;
+    }
+    SCORM_API["LMSInitialize"]("");
+  },
+  get(key: string, defaultValue?: any) {
+    if (SCORM_API === null) {
+      return;
+    }
+    const value = SCORM_API["LMSGetValue"](key);
+    if (key === "cmi.suspend_data") {
+      try {
+        if (value === "") {
+          return defaultValue;
+        }
+        return JSON.parse(value) ?? defaultValue;
+      } catch {
+        return defaultValue;
+      }
+    }
+    if (value === "") {
+      return defaultValue;
+    }
+    return value;
+  },
+  set(key: string, value: any) {
+    if (SCORM_API === null) {
+      return;
+    }
+    SCORM_API["LMSSetValue"](key, key === "cmi.suspend_data" ? JSON.stringify(value) : value);
+  },
+  commit() {
+    if (SCORM_API === null) {
+      return;
+    }
+    SCORM_API["LMSCommit"]("");
+  },
+  terminate() {
+    if (SCORM_API === null) {
+      return;
+    }
+    SCORM_API["LMSFinish"]("");
+  },
+};
